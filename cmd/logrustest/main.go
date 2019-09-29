8f3b4710e59c1d04d9311c0a218c04b3ed7a8ff1
YonghoChoi <yongho1037@gmail.com>
2019-09-29 16:23:27 +0900

    logrus에 roation 기능 추가

--- cmd/logrustest/main.go
+++ cmd/logrustest/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"os"
 )
 
@@ -19,6 +20,23 @@ func main() {
 			TimestampFormat: "2006-01-02 15:04:05",
 			FullTimestamp:   true,
 		},
+	}
+
+	l := &lumberjack.Logger{
+		Filename:   "./test.log",
+		MaxSize:    100, // megabytes
+		MaxBackups: 10,
+		MaxAge:     1,    //days
+		Compress:   true, // disabled by default
+	}
+
+	logger.SetOutput(l)
+	logger.Debugf("test")
+	logger.Debugf("test")
+	logger.Debugf("test")
+	if err := l.Rotate(); err != nil {
+		logger.Error(err.Error())
+		return
 	}
 
 	logger.Debugf("test")
